1986857fe5146a0edda276a9ca943aabccae9306
yukikimoto <103980824+yukikimoto@users.noreply.github.com>
2024-07-16 10:40:38 +0900

    ログイン機能とログアウト機能、サインアップ機能の確認

--- src/ai_chatapp/settings.py
+++ src/ai_chatapp/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# tech_chat/settings.py
+
+LOGIN_REDIRECT_URL = '/'
--- src/ai_chatapp/urls.py
+++ src/ai_chatapp/urls.py
@@ -21,6 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
